1f8bd9c9236797ab9066ac00a71e91014f20f568
qsceszasd <umikchan@gmail.com>
2023-03-22 23:12:10 +0900

    바보바보
    ㅎㅇㅎㅇ

--- src/main/java/shop/controller/user/UserController.java
+++ src/main/java/shop/controller/user/UserController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class UserController {
 
 	public void dwmwm() {
-		System.out.println("sdjsdskdj");
+		System.out.println("바보바보바보");
+		System.out.println("너희들은 바보야");
+		System.out.println("랄랄라 랄랄라 ");
 	}
 	
 }
