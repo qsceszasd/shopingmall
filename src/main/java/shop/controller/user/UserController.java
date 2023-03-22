a8584abdb67069d170bbc6822f5a18a50857687f
zrmin <zrminkim@gmail.com>
2023-03-22 22:59:57 +0900

    second commit

---
+++ src/main/java/shop/controller/user/UserController.java
@@ -0,0 +1,13 @@
+package shop.controller.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+	public void dwmwm() {
+		System.out.println("sdjsdskdj");
+	}
+	
+}
